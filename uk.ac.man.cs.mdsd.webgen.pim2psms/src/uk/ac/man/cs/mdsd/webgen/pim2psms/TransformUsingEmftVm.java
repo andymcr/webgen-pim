8ea850e2eedc772a20372ae4ac0221c88feebce0
Andy Carpenter <Andy.Carpenter@manchester.ac.uk>
2015-05-13 13:48:16 +0100

    Update to reflect name change for framework model

--- uk.ac.man.cs.mdsd.webgen.pim2psms/src/uk/ac/man/cs/mdsd/webgen/pim2psms/TransformUsingEmftVm.java
+++ uk.ac.man.cs.mdsd.webgen.pim2psms/src/uk/ac/man/cs/mdsd/webgen/pim2psms/TransformUsingEmftVm.java
@@ -20,7 +20,7 @@ public class TransformUsingEmftVm {
 	private ResourceSet resourceSet = new ResourceSetImpl();
 	private Metamodel websiteMetamodel;
 	private Metamodel ormMetamodel;
-	private Metamodel jsfMetamodel;
+	private Metamodel wafMetamodel;
 	private Model websiteModel;
 	private ModuleResolver moduleResolver;
 
@@ -44,14 +44,14 @@ public class TransformUsingEmftVm {
 		return ormMetamodel;
 	}
 
-	protected Metamodel getJsfMetamodel() {
-		if (jsfMetamodel == null) {
-			jsfMetamodel = EmftvmFactory.eINSTANCE.createMetamodel();
-			jsfMetamodel.setResource(resourceSet.getResource(
-				URI.createURI("http://www.cs.man.ac.uk/mdsd/2010/Jsf"), true));
+	protected Metamodel getWafMetamodel() {
+		if (wafMetamodel == null) {
+			wafMetamodel = EmftvmFactory.eINSTANCE.createMetamodel();
+			wafMetamodel.setResource(resourceSet.getResource(
+				URI.createURI("http://www.cs.man.ac.uk/mdsd/2015/WebApplicationFramework"), true));
 		}
 
-		return jsfMetamodel;
+		return wafMetamodel;
 	}
 
 	public Model getWebsiteModel() {
@@ -88,7 +88,7 @@ xxx = websiteModelFile.getFullPath().toString();
 		final ExecEnv env = EmftvmFactory.eINSTANCE.createExecEnv();
 		env.registerMetaModel("Website", getWebsiteMetamodel());
 		env.registerMetaModel("ORM", getOrmMetamodel());
-		env.registerMetaModel("JSF", getJsfMetamodel());
+		env.registerMetaModel("WAF", getWafMetamodel());
 
 		return env;
 		
@@ -126,7 +126,7 @@ xxx = websiteModelFile.getFullPath().toString();
 				traceModel);
 	}
 
-	protected Model executeJsfPass(final String moduleName,
+	protected Model executeWafPass(final String moduleName,
 			final String modelFileExtension, final Model partialModel,
 			final Model ormModel, final Model traceModel) {
 		final ExecEnv env = createEnvironment();
@@ -134,10 +134,10 @@ xxx = websiteModelFile.getFullPath().toString();
 		env.registerInputModel("orm", ormModel);
 
 		if (partialModel != null) {
-			env.registerInputModel("partialJsf", partialModel);
+			env.registerInputModel("partialWaf", partialModel);
 		}
 
-		return executePass(env, moduleName, "jsf", modelFileExtension,
+		return executePass(env, moduleName, "waf", modelFileExtension,
 				traceModel);
 	}
 
@@ -153,38 +153,38 @@ xxx = websiteModelFile.getFullPath().toString();
 		ormModel.getResource().save(Collections.emptyMap());
 
 		final Model traceModelP1 = createModel("_trace_p1");
-		final Model jsfModelP1 = executeJsfPass("PassExplicitInterface",
-			"_jsf_p1", null, ormModel, traceModelP1);
+		final Model wafModelP1 = executeWafPass("PassExplicitInterface",
+			"_waf_p1", null, ormModel, traceModelP1);
 
 		final Model traceModelP2 = createModel("_trace_p2");
-		final Model jsfModelP2 = executeJsfPass("PassImplicitService",
-			"_jsf_p2", jsfModelP1, ormModel, traceModelP2);
+		final Model wafModelP2 = executeWafPass("PassImplicitService",
+			"_waf_p2", wafModelP1, ormModel, traceModelP2);
 
 		final Model traceModelP3 = createModel("_trace_p3");
-		final Model jsfModelP3 = executeJsfPass(
-			"PassImplicitServiceEntityFeatures", "_jsf_p3", jsfModelP2,
+		final Model wafModelP3 = executeWafPass(
+			"PassImplicitServiceEntityFeatures", "_waf_p3", wafModelP2,
 			ormModel, traceModelP3);
 
 		final Model traceModelP4 = createModel("_trace_p4");
-		final Model jsfModelP4 = executeJsfPass("PassImplicitServiceUse",
-			"_jsf_p4", jsfModelP3, ormModel, traceModelP4);
+		final Model wafModelP4 = executeWafPass("PassImplicitServiceUse",
+			"_waf_p4", wafModelP3, ormModel, traceModelP4);
 
 		final Model traceModelP5 = createModel("_trace_p5");
-		final Model jsfModelP5 = executeJsfPass("PassImplicitStaticInterface",
-			"_jsf_p5", jsfModelP4, ormModel, traceModelP5);
+		final Model wafModelP5 = executeWafPass("PassImplicitStaticInterface",
+			"_waf_p5", wafModelP4, ormModel, traceModelP5);
 
 		final Model traceModelP6 = createModel("_trace_p6");
-		final Model jsfModelP6 = executeJsfPass("PassImplicitUnitFields",
-			"_jsf_p6", jsfModelP5, ormModel, traceModelP6);
+		final Model wafModelP6 = executeWafPass("PassImplicitUnitFields",
+			"_waf_p6", wafModelP5, ormModel, traceModelP6);
 
 		final Model traceModelP7 = createModel("_trace_p7");
-		final Model jsfModelP7 = executeJsfPass("PassAuthentication",
-			"_jsf_p7", jsfModelP6, ormModel, traceModelP7);
+		final Model wafModelP7 = executeWafPass("PassAuthentication",
+			"_waf_p7", wafModelP6, ormModel, traceModelP7);
 
 		final Model traceModelP8 = createModel("_trace_p8");
-		final Model jsfModel = executeJsfPass("PassAuthentication2",
-			"_jsf", jsfModelP7, ormModel, traceModelP8);
-		jsfModel.getResource().save(Collections.emptyMap());
+		final Model wafModel = executeWafPass("PassAuthentication2",
+			"_waf", wafModelP7, ormModel, traceModelP8);
+		wafModel.getResource().save(Collections.emptyMap());
 	}
 
 }
