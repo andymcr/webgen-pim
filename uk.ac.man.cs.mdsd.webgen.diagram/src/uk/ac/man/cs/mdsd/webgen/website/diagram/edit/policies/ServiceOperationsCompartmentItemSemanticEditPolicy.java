d5befd7c35dc061bf679ca85e1309178bd0bbd01
Andy Carpenter <Andy.Carpenter@manchester.ac.uk>
2016-05-06 13:06:18 +0100

    Allow user defined operations in services

---
+++ uk.ac.man.cs.mdsd.webgen.diagram/src/uk/ac/man/cs/mdsd/webgen/website/diagram/edit/policies/ServiceOperationsCompartmentItemSemanticEditPolicy.java
@@ -0,0 +1,31 @@
+package uk.ac.man.cs.mdsd.webgen.website.diagram.edit.policies;
+
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
+
+import uk.ac.man.cs.mdsd.webgen.website.diagram.edit.commands.BusinessOperationCreateCommand;
+import uk.ac.man.cs.mdsd.webgen.website.diagram.providers.WebsiteElementTypes;
+
+/**
+ * @generated
+ */
+public class ServiceOperationsCompartmentItemSemanticEditPolicy extends WebsiteBaseItemSemanticEditPolicy {
+
+	/**
+	* @generated
+	*/
+	public ServiceOperationsCompartmentItemSemanticEditPolicy() {
+		super(WebsiteElementTypes.Service_2004);
+	}
+
+	/**
+	* @generated
+	*/
+	protected Command getCreateCommand(CreateElementRequest req) {
+		if (WebsiteElementTypes.BusinessOperation_3260 == req.getElementType()) {
+			return getGEFWrapper(new BusinessOperationCreateCommand(req));
+		}
+		return super.getCreateCommand(req);
+	}
+
+}
