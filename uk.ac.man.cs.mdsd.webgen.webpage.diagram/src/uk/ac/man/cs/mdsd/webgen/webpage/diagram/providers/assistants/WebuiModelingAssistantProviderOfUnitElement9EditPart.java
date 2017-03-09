1673ea4f16a7745541fc9c155f3476ba50afe940
andymcr <Andy Carpenter>
2017-03-09 13:50:32 +0000

    Files for unit fields

---
+++ uk.ac.man.cs.mdsd.webgen.webpage.diagram/src/uk/ac/man/cs/mdsd/webgen/webpage/diagram/providers/assistants/WebuiModelingAssistantProviderOfUnitElement9EditPart.java
@@ -0,0 +1,10 @@
+package uk.ac.man.cs.mdsd.webgen.webpage.diagram.providers.assistants;
+
+import uk.ac.man.cs.mdsd.webgen.webpage.diagram.providers.WebuiModelingAssistantProvider;
+
+/**
+ * @generated
+ */
+public class WebuiModelingAssistantProviderOfUnitElement9EditPart extends WebuiModelingAssistantProvider {
+
+}
