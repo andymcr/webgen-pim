63114b6c0352d22d69e11e3ab6d8410fa3512817
andymcr <Andy Carpenter>
2017-06-16 15:14:14 +0100

    Avoid showing readonly names in properties list

--- uk.ac.man.cs.mdsd.webgen.base.edit/src/uk/ac/man/cs/mdsd/webgen/base/provider/ParameterReferenceItemProvider.java
+++ uk.ac.man.cs.mdsd.webgen.base.edit/src/uk/ac/man/cs/mdsd/webgen/base/provider/ParameterReferenceItemProvider.java
@@ -62,32 +62,9 @@ public class ParameterReferenceItemProvider
 		if (itemPropertyDescriptors == null) {
 			super.getPropertyDescriptors(object);
 
-			addNamePropertyDescriptor(object);
 			addParameterPropertyDescriptor(object);
 		}
 		return itemPropertyDescriptors;
-	}
-
-	/**
-	 * This adds a property descriptor for the Name feature.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	protected void addNamePropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_ParameterReference_name_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_ParameterReference_name_feature", "_UI_ParameterReference_type"),
-				 BasePackage.Literals.PARAMETER_REFERENCE__NAME,
-				 false,
-				 false,
-				 false,
-				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
-				 getString("_UI_ModelPropertyCategory"),
-				 null));
 	}
 
 	/**
