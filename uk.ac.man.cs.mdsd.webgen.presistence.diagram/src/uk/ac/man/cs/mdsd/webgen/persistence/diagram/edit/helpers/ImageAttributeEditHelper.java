10070602d59101b934309308b6bb0f869cb145d7
andymcr <Andy Carpenter>
2018-02-16 13:23:57 +0000

    Remove projects using legacy GMF

--- uk.ac.man.cs.mdsd.webgen.presistence.diagram/src/uk/ac/man/cs/mdsd/webgen/persistence/diagram/edit/helpers/ImageAttributeEditHelper.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.man.cs.mdsd.webgen.persistence.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class ImageAttributeEditHelper extends PersistenceBaseEditHelper {
-}
--- uk.ac.man.cs.mdsd.webgen.webpage.diagram/src/uk/ac/man/cs/mdsd/webgen/webpage/diagram/edit/helpers/StaticUnitEditHelper.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.man.cs.mdsd.webgen.webpage.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class StaticUnitEditHelper extends WebuiBaseEditHelper {
-}
