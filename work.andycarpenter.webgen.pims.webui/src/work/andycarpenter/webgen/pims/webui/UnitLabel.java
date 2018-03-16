415378b2695b5418e6ae61434cac2abea74aa599
andymcr <Andy Carpenter>
2018-03-16 14:19:20 +0000

    Allow use of entity labels as unit fields

---
+++ work.andycarpenter.webgen.pims.webui/src/work/andycarpenter/webgen/pims/webui/UnitLabel.java
@@ -0,0 +1,67 @@
+/**
+ */
+package work.andycarpenter.webgen.pims.webui;
+
+import work.andycarpenter.webgen.pims.persistence.ModelLabel;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Unit Label</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * </p>
+ * <ul>
+ *   <li>{@link work.andycarpenter.webgen.pims.webui.UnitLabel#getLabel <em>Label</em>}</li>
+ *   <li>{@link work.andycarpenter.webgen.pims.webui.UnitLabel#getName <em>Name</em>}</li>
+ * </ul>
+ *
+ * @see work.andycarpenter.webgen.pims.webui.WebuiPackage#getUnitLabel()
+ * @model
+ * @generated
+ */
+public interface UnitLabel extends UnitField {
+	/**
+	 * Returns the value of the '<em><b>Label</b></em>' reference.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Label</em>' reference isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Label</em>' reference.
+	 * @see #setLabel(ModelLabel)
+	 * @see work.andycarpenter.webgen.pims.webui.WebuiPackage#getUnitLabel_Label()
+	 * @model required="true"
+	 * @generated
+	 */
+	ModelLabel getLabel();
+
+	/**
+	 * Sets the value of the '{@link work.andycarpenter.webgen.pims.webui.UnitLabel#getLabel <em>Label</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Label</em>' reference.
+	 * @see #getLabel()
+	 * @generated
+	 */
+	void setLabel(ModelLabel value);
+
+	/**
+	 * Returns the value of the '<em><b>Name</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Name</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Name</em>' attribute.
+	 * @see work.andycarpenter.webgen.pims.webui.WebuiPackage#getUnitLabel_Name()
+	 * @model changeable="false" volatile="true" derived="true" ordered="false"
+	 *        annotation="http://www.eclipse.org/emf/2002/Ecore/OCL derivation='if label.oclIsUndefined() then\r\n\t\'\'\r\nelse\r\n\tlabel.name\r\nendif\r\n'"
+	 * @generated
+	 */
+	String getName();
+
+} // UnitLabel
---
+++ work.andycarpenter.webgen.pims.webui/src/work/andycarpenter/webgen/pims/webui/impl/UnitLabelImpl.java
@@ -0,0 +1,182 @@
+/**
+ */
+package work.andycarpenter.webgen.pims.webui.impl;
+
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+
+import work.andycarpenter.webgen.pims.persistence.ModelLabel;
+
+import work.andycarpenter.webgen.pims.webui.UnitLabel;
+import work.andycarpenter.webgen.pims.webui.WebuiPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Unit Label</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * </p>
+ * <ul>
+ *   <li>{@link work.andycarpenter.webgen.pims.webui.impl.UnitLabelImpl#getLabel <em>Label</em>}</li>
+ *   <li>{@link work.andycarpenter.webgen.pims.webui.impl.UnitLabelImpl#getName <em>Name</em>}</li>
+ * </ul>
+ *
+ * @generated
+ */
+public class UnitLabelImpl extends UnitFieldImpl implements UnitLabel {
+	/**
+	 * The cached value of the '{@link #getLabel() <em>Label</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getLabel()
+	 * @generated
+	 * @ordered
+	 */
+	protected ModelLabel label;
+
+	/**
+	 * The cached setting delegate for the '{@link #getName() <em>Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getName()
+	 * @generated
+	 * @ordered
+	 */
+	protected EStructuralFeature.Internal.SettingDelegate NAME__ESETTING_DELEGATE = ((EStructuralFeature.Internal)WebuiPackage.Literals.UNIT_LABEL__NAME).getSettingDelegate();
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected UnitLabelImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return WebuiPackage.Literals.UNIT_LABEL;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ModelLabel getLabel() {
+		if (label != null && label.eIsProxy()) {
+			InternalEObject oldLabel = (InternalEObject)label;
+			label = (ModelLabel)eResolveProxy(oldLabel);
+			if (label != oldLabel) {
+				if (eNotificationRequired())
+					eNotify(new ENotificationImpl(this, Notification.RESOLVE, WebuiPackage.UNIT_LABEL__LABEL, oldLabel, label));
+			}
+		}
+		return label;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ModelLabel basicGetLabel() {
+		return label;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setLabel(ModelLabel newLabel) {
+		ModelLabel oldLabel = label;
+		label = newLabel;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, WebuiPackage.UNIT_LABEL__LABEL, oldLabel, label));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getName() {
+		return (String)NAME__ESETTING_DELEGATE.dynamicGet(this, null, 0, true, false);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case WebuiPackage.UNIT_LABEL__LABEL:
+				if (resolve) return getLabel();
+				return basicGetLabel();
+			case WebuiPackage.UNIT_LABEL__NAME:
+				return getName();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case WebuiPackage.UNIT_LABEL__LABEL:
+				setLabel((ModelLabel)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case WebuiPackage.UNIT_LABEL__LABEL:
+				setLabel((ModelLabel)null);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case WebuiPackage.UNIT_LABEL__LABEL:
+				return label != null;
+			case WebuiPackage.UNIT_LABEL__NAME:
+				return NAME__ESETTING_DELEGATE.dynamicIsSet(this, null, 0);
+		}
+		return super.eIsSet(featureID);
+	}
+
+} //UnitLabelImpl
