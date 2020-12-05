2603b919fdb22bcba4aa14d71984c3e286d1ca58
Andy Carpenter <6350464+andymcr@users.noreply.github.com>
2020-12-05 00:36:32 +0000

    Simplify determination of parent types for feature references

--- work.andycarpenter.webgen.pims.webui.edit/src/work/andycarpenter/webgen/pims/webui/provider/FeaturePathItemProvider.java
+++ work.andycarpenter.webgen.pims.webui.edit/src/work/andycarpenter/webgen/pims/webui/provider/FeaturePathItemProvider.java
@@ -11,9 +11,7 @@ import java.util.Set;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.ResourceLocator;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
@@ -27,21 +25,13 @@ import org.eclipse.emf.edit.provider.ViewerNotification;
 
 import work.andycarpenter.webgen.pims.expression.Expression;
 import work.andycarpenter.webgen.pims.expression.ExpressionPackage;
-import work.andycarpenter.webgen.pims.persistence.Association;
-import work.andycarpenter.webgen.pims.persistence.AssociationWithContainment;
-import work.andycarpenter.webgen.pims.persistence.Attribute;
 import work.andycarpenter.webgen.pims.persistence.Entity;
-import work.andycarpenter.webgen.pims.service.Selection;
 import work.andycarpenter.webgen.pims.webui.Badge;
 import work.andycarpenter.webgen.pims.webui.CollectionUnit;
 import work.andycarpenter.webgen.pims.webui.DynamicUnit;
 import work.andycarpenter.webgen.pims.webui.FeaturePath;
-import work.andycarpenter.webgen.pims.webui.FeaturePathAssociation;
 import work.andycarpenter.webgen.pims.webui.InlineAction;
-import work.andycarpenter.webgen.pims.webui.InlineActionContainer;
 import work.andycarpenter.webgen.pims.webui.SingletonUnit;
-import work.andycarpenter.webgen.pims.webui.UnitAssociation;
-import work.andycarpenter.webgen.pims.webui.UnitElement;
 import work.andycarpenter.webgen.pims.webui.UnitField;
 import work.andycarpenter.webgen.pims.webui.WebuiPackage;
 
@@ -186,249 +176,83 @@ public class FeaturePathItemProvider
 
 	protected Set<Entity> getParentTypes(final FeaturePath path) {
 		if (path.eContainer() instanceof DynamicUnit) {
-			return getContentTypes((DynamicUnit) path.eContainer(), isOnCollectionUnit(path));
+			return getParentTypes((DynamicUnit) path.eContainer());
+
+		} else if (path.eContainer() instanceof UnitField) {
+			return getParentTypes((UnitField) path.eContainer());
 
 		} else if (path.eContainer() instanceof InlineAction) {
-			return getEntities((InlineActionContainer) path.eContainer().eContainer());
-
-		} else if (path.eContainer() instanceof Expression) {
-			return getEntities((Expression) path.eContainer(), isOnCollectionUnit(path));
+			return getParentTypes((InlineAction) path.eContainer());
 
 		} else if (path.eContainer() instanceof Badge) {
-			return getEntities((Badge) path.eContainer());
-
-		} else {
-			return Collections.emptySet();
-		}
-	}
-
-	protected Boolean isOnCollectionUnit(EObject eObject) {
-		if (eObject.eContainer() == null) {
-			return false;
-		} else if (eObject.eContainer() instanceof CollectionUnit) {
-			return true;
-		} else {
-			return isOnCollectionUnit(eObject.eContainer());
-		}
-	}
-
-	protected Set<Entity> getTargets(final FeaturePathAssociation path) {
-		if (path.eContainer() instanceof DynamicUnit) {
-			final Set<Entity> targets = new HashSet<Entity>();
-			targets.add(getTarget(path.getAssociation(), (DynamicUnit) path.eContainer()));
-			return targets;
-
-		} else if (path.eContainer() instanceof InlineAction) {
-			final Set<Entity> targets = new HashSet<Entity>();
-			final Set<Entity> entities
-				= getEntities((InlineActionContainer) path.eContainer().eContainer());
-			if (entities.contains(path.getAssociation().getPartOf())) {
-				targets.add(path.getAssociation().getTargetEntity());
-			} else{
-				targets.add(path.getAssociation().getPartOf());
-			}
-			return targets;
+			return getParentTypes((Badge) path.eContainer());
 
 		} else if (path.eContainer() instanceof Expression) {
-			return getEntities((Expression) path.eContainer());
+			return getParentTypes((Expression) path.eContainer());
 
 		} else {
 			return Collections.emptySet();
 		}
 	}
 
-	protected Entity getTarget(final Association association, final DynamicUnit unit) {
-		final Set<Entity> contentType = getContentTypes(unit);
-		if (contentType.contains(association.getPartOf())) {
-			return association.getTargetEntity();
-		} else{
-			return association.getPartOf();
-		}
-	}
-
-	protected Set<Entity> getContentTypes(final DynamicUnit unit) {
-		return getContentTypes(unit, false);
-	}
-
-	protected Set<Entity> getContentTypes(final DynamicUnit unit, final Boolean useContainer) {
-		final Set<Entity> contentType = new HashSet<Entity>();
-
+	protected Set<Entity> getParentTypes(final DynamicUnit unit) {
+		final Set<Entity> parentTypes = new HashSet<Entity>();
 		if (unit instanceof SingletonUnit) {
 			final SingletonUnit singleton = (SingletonUnit) unit;
 			if (singleton.getContentType() != null) {
-				contentType.add(singleton.getContentType());
-				return contentType;
+				parentTypes.add(singleton.getContentType());
 			}
 		}
 
 		if (unit instanceof CollectionUnit) {
 			final CollectionUnit collection = (CollectionUnit) unit;
-			if (useContainer) {
-				final EList<Entity> contentType2 = ((CollectionUnit) unit).getContentType();
-				if (contentType2.size() > 0) {
-					final Association association
-						= getContainingAssociation(contentType2.get(0));
-					if (association != null) {
-						contentType.add(association.getPartOf());
-					}
-				}
-			} else {
-				contentType.addAll(collection.getContentType());
-				if ((collection.getSelection() != null)
-						&& (collection.getSelection().getSelectPath().size() > 0)) {
-					contentType.add(getSelectType(collection.getSelection(), collection.getContentType()));
-				}
-			}
-			return contentType;
-		}
-
-		return contentType;
-	}
-
-	protected AssociationWithContainment getContainingAssociation(final Entity type) {
-		for (Association association : type.getAllAssociations()) {
-			if (association instanceof AssociationWithContainment) {
-				if (!type.getAssociations().contains(association)) {
-					return (AssociationWithContainment) association;
-				}
+			if (collection.getContainingType() != null) {
+				parentTypes.add(collection.getContainingType());
 			}
 		}
 
-		return null;
+		return parentTypes;
 	}
 
-	protected Entity getSelectType(final Selection selection, final List<Entity> parentTypes) {
-		Entity type = null;
-		boolean first = true;
-		for (Association association : selection.getSelectPath()) {
-			if (first) {
-				if (parentTypes.contains(association.getPartOf())) {
-					type = association.getTargetEntity();
-				} else {
-					type = association.getPartOf();
-				}
-			} else {
-				if (association.getPartOf().equals(type)) {
-					type = association.getTargetEntity();
-				} else {
-					type = association.getPartOf();
-				}
-			}
-			first = false;
+	protected Set<Entity> getParentTypes(final UnitField field) {
+		return getParentTypes(field.getDisplayedOn());
+	}
+
+	protected Set<Entity> getParentTypes(final InlineAction action) {
+		final Set<Entity> parentTypes = new HashSet<Entity>();
+		if (action.getUsedBy() instanceof CollectionUnit) {
+			final CollectionUnit unit = (CollectionUnit) action.getUsedBy();
+			parentTypes.addAll(getParentTypes(unit));
+		} else if (action.getUsedBy() instanceof UnitField) {
+			final UnitField field = (UnitField) action.getUsedBy();
+			parentTypes.addAll(getParentTypes(field));
 		}
-
-		return type;
+		return parentTypes;
 	}
 
-	protected Set<Entity> getEntities(final InlineActionContainer container) {
-		final Set<Entity> entities = new HashSet<Entity>();
-		if (container instanceof CollectionUnit) {
-			entities.addAll(((CollectionUnit) container).getContentType());
-		} else if (container instanceof UnitElement) {
-			entities.add(getParentType(
-				((UnitElement) container).getAttribute()));
-		} else if (container instanceof UnitAssociation) {
-			entities.add(((UnitAssociation) container).getAssociation().getPartOf());
-		}
-
-		return entities;
+	protected Set<Entity> getParentTypes(final Badge badge) {
+		return getParentTypes(badge.getDisplayedOn());
 	}
 
-	protected Set<Entity> getEntities(final Badge badge) {
-		final Set<Entity> entities = new HashSet<Entity>();
-		if (badge.eContainer() instanceof CollectionUnit) {
-			entities.addAll(((CollectionUnit) badge.eContainer()).getContentType());
-		} else if (badge.eContainer() instanceof UnitElement) {
-			entities.add(getParentType(
-				((UnitElement) badge.eContainer()).getAttribute()));
-		} else if (badge.eContainer() instanceof UnitAssociation) {
-			entities.add(((UnitAssociation) badge.eContainer()).getAssociation().getPartOf());
-		}
+	protected Set<Entity> getParentTypes(final Expression expression) {
+		if (expression.eContainer() instanceof DynamicUnit) {
+			return getParentTypes((DynamicUnit) expression.eContainer());
 
-		return entities;
-	}
+		} else if (expression.eContainer() instanceof UnitField) {
+			return getParentTypes((UnitField) expression.eContainer());
 
-	protected Set<Entity> getEntities(final Expression expression) {
-		return getEntities(expression, false);
-	}
+		} else if (expression.eContainer() instanceof InlineAction) {
+			return getParentTypes((InlineAction) expression.eContainer());
 
-	protected Set<Entity> getEntities(final Expression expression, final Boolean useContainer) {
-		final DynamicUnit unit = getDynamicUnitContext(expression);
-		if (unit != null) {
-			return getContentTypes(unit, useContainer);
-		}
+		} else if (expression.eContainer() instanceof Badge) {
+			return getParentTypes((Badge) expression.eContainer());
 
-		final InlineActionContainer action = getActionContext(expression);
-		if (action != null) {
-			return getEntities(action);
-		}
+		} else if (expression.eContainer() instanceof Expression) {
+			return getParentTypes((Expression) expression.eContainer());
 
-		final Badge badge = getBadgeContext(expression);
-		if (badge != null) {
-			return getEntities(badge);
-		}
-
-		return Collections.emptySet();
-	}
-
-	protected Entity getParentType(final Attribute attribute) {
-		return attribute.getPartOf();
-	}
-
-	protected Object getContext(final Object object) {
-		if (object instanceof EObject) {
-			return ((EObject) object).eContainer();
 		} else {
-			return null;
+			return Collections.emptySet();
 		}
 	}
 
-	protected DynamicUnit getDynamicUnitContext(final Object object) {
-		Object container = getContext(object);
-		while (container != null) {
-			if (container instanceof DynamicUnit) {
-				return (DynamicUnit) container;
-			}
-			container = getContext(container);
-		}
-
-		return null;
-	}
-
-	protected UnitField getUnitFieldContext(final Object object) {
-		Object container = getContext(object);
-		while (container != null) {
-			if (container instanceof UnitField) {
-				return (UnitField) container;
-			}
-			container = getContext(container);
-		}
-
-		return null;
-	}
-
-	protected InlineActionContainer getActionContext(final Object object) { 
-	Object container = getContext(object); 
- 		while (container != null) { 
- 			if (container instanceof InlineActionContainer) { 
- 				return (InlineActionContainer) container; 
- 			} 
- 			container = getContext(container); 
- 		} 
- 
- 		return null; 
-	}
- 
-	protected Badge getBadgeContext(final Object object) { 
-	Object container = getContext(object); 
- 		while (container != null) { 
- 			if (container instanceof Badge) { 
- 				return (Badge) container; 
- 			} 
- 			container = getContext(container); 
- 		} 
- 
- 		return null; 
-	}
- 
 }
